dcf389c24c1e00eb842873aab7c6cce5d6902f61
nicolassalasa <nsalas@saltala.com>
2018-07-10 17:40:20 -0400

    Creacion Front INICIO

---
+++ src/components/Inicio/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Card, Button, Carousel } from 'antd';
+import { Link } from 'react-router-dom';
+import pic from '../../images/Inicio.png';
+import { cardstyle } from '../globalcss';
+import { VISION, MISION, WELCOME_MESSAGE, EMPATIA } from '../../constants';
+import { Div1, Div2, Div3, DivContent, DivContent2, Divh3, Divh4, Divh42 } from './style';
+import './style.css';
+import ReactHtmlParser from 'react-html-parser';
+
+class Inicio extends React.Component {
+    render() {
+        return (
+
+            <div>
+                <body className='Inicio '>
+                    <Link to='/' className='Botton'>
+                        <Button type="primary" style={{ marginTop: 30 }}>
+                            Comenzar
+                   </Button>
+                    </Link>
+                </body>
+
+            </div>
+
+
+
+        );
+    }
+}
+
+export default Inicio;
+{/* <Card
+                title={WELCOME_MESSAGE.FROM_REGISTER}
+                bordered={false}
+                style={cardstyle}>
+                <Carousel >
+
+                    <Div1>
+
+                        <DivContent>
+                            <Divh3> {VISION.TITLE} </Divh3>
+                            <Divh4> {ReactHtmlParser(VISION.RESUMEN)} </Divh4>
+                        </DivContent>
+
+                    </Div1>
+
+                    <Div2>
+                        <DivContent2>
+                            <Divh3> {MISION.TITLE} </Divh3>
+                            <Divh42> {ReactHtmlParser(MISION.RESUMEN)} </Divh42>
+                        </DivContent2>
+                    </Div2>
+
+                    <Div3>
+                        <DivContent>
+                            <Divh3> {EMPATIA.TITLE} </Divh3>
+                            <Divh4> {ReactHtmlParser(EMPATIA.RESUMEN)} </Divh4>
+                        </DivContent>
+                    </Div3>
+
+                </Carousel>
+                <Link to='/registro'>
+                    <Button type="primary" style={{ marginTop: 30 }}>
+                        Comenzar
+                   </Button>
+                </Link>
+            </Card > */}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch, HashRouter } from 'react-router-dom';
 
-import Welcome from '../components/welcome';
+import Welcome from '../components/Welcome';
 import Thanks from '../components/thanks/index';
 import NotFound from '../components/NotFound/index';
 import Register from '../components/Register/index';
 import RegisterMail from '../components/Register/saveMail/index';
 import GetMail from '../components/Questionary/1_/GetEmail'
+import Inicio from '../components/Inicio';
 
 import Questionary_1 from '../components/Questionary/1_';
 import key_validator from '../components/Questionary/1_/valid'
@@ -16,7 +17,8 @@ import Questionary_3 from '../components/Questionary/3_';
 export default () => (
 	<HashRouter>
 		<Switch>
-			<Route path="/" exact component={Welcome} /> 
+			<Route path="/" exact component={Welcome} />
+			<Route path="/Inicio" exact component={Inicio} />
 			<Route path="/Bienvenido/:id" exact component={key_validator} />
 			<Route path="/Cuestionario" exact component={Questionary_1} />
 			<Route path="/Seleccion" exact component={Questionary_2} />
