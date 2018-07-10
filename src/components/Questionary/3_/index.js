9b6aa7b55953d1f4cf9beae6d968cb1b67228306
Hernan_ <hernanh@saltala.com>
2018-07-10 19:44:38 -0400

    100

--- src/components/Questionary/3_/index.js
+++ src/components/Questionary/3_/index.js
@@ -71,6 +71,8 @@ class GETQuestion extends React.Component {
         let all = await this.props.companyReducers.AllTheAnswer;
         all.FreeQuestion = Object.values(value)[0];
 
+        console.log('AAAAAALL',all)
+
         await this.props.onOpenQuestionChanged(all);
 
         // _________________________OK
