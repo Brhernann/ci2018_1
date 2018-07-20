f9324bdbcce3314a19cef5f656bd325d1ae96c38
nicolassalasa <nsalas@saltala.com>
2018-07-19 22:35:01 -0400

    cambios

--- src/components/welcome/style.js
+++ src/components/welcome/style.js
@@ -26,28 +26,24 @@ import Artboard3 from './pics/Artboard3.png';
 /* .carousel-caption {
     padding-bottom: 100px;
 }
-
 .ant-carousel.slick-slide {
     text-align: center;
     height: 160px;
     line-height: 160px;
     overflow: hidden;
 }
-
 .ant-carousel.slick-slide h3 {
     color: #fff;
     position: absolute;
 }
-
 .ant-card-body {
     margin-left: 5%;
 } */
 
 export const Div1 = styled.div`
   background-image: url(${Artboard});
-  background
   ${media.phone` 
-  background-image: url(${ArtboardPhone});
+  background-image: url(${Artboard3});
 `}
 `;
 
@@ -79,20 +75,26 @@ export const DivContent2 = styled.div`
 
 export const Divh3 = styled.h3`
 margin-top:1vw;
-padding:1vw;
 color:white;
-font-weight:100;
-text-align:justify;
-font-size:85%;
+font-style:oblique;
+text-decoration: underline;
+${media.phone` 
+font-size:90%;
+margin-top:3vw;
+margin-bottom:15vw;
+`};
 `;
 
 export const Divh4 = styled.h4`
 padding:1vw;
-padding-right:6vw;
+padding-right: 3vw;
 color:white;
 font-weight:100;
 text-align:justify;
-font-size:85%;
+font-size:100%;
+${media.phone` 
+font-size:75%;
+`};
 `;
 
 export const Divh42 = styled.h4`
@@ -100,7 +102,10 @@ padding:1vw;
 color:white;
 font-weight:100;
 text-align:justify;
-font-size:85%
+font-size:100%;
+${media.phone` 
+font-size:85%;
+`};
 `;
 
 /* .slide {
