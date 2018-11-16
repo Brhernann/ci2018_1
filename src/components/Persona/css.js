47f634797ed26f7f44f01ec9a87e2efe61eab5f7
nicolasSalas <npsa1794@gmail.com>
2018-11-15 21:43:06 -0300

    creacion de componente para inscripcion por persona natural

---
+++ src/components/Persona/css.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import media from '../media';
+
+export const FirstContent = styled.div `
+    display: flex;
+    justify-content:center;
+`
+
+export const FirstChild = styled.div `
+width:50%;
+text-align:left;
+${media.phone` 
+width:100%;
+font-size:10px;
+`}
+`;
+
+export const FormContent = styled.div`
+display:flex;
+flex-direction:column;
+width:100%;
+align-items:center;
+`;
